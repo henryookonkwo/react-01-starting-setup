897030aa10bce96b79374a693f814047f625a59d
Henry Okonkwo <henryokonkwo@henrys-mbp.home>
2023-06-26 23:55:19 -0400

    passing data via props and splitting components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,31 @@
-function ExpenseItem() {}
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+//using object destructuring
+
+// function ExpenseItem({ date, amount, title }) {
+//   //destructuing
+//   return (
+//     <div className="expense-item">
+//       <div>{date.toISOString()}</div>
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">${amount}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default ExpenseItem;
