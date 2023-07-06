53ffa3f916b5c3357b6c0d821a55affb79e541a7
Henry Okonkwo <henryokonkwo@henrys-mbp.home>
2023-07-05 21:21:39 -0400

    add Form elements to application

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,8 +7,8 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    setTitle("Updated");
-    console.log("Clicked");
+    setTitle("Updated!");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
