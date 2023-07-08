695cd2be435773806855246a2a163bc4d37c902a
Henry Okonkwo <henryokonkwo@henrys-mbp.home>
2023-07-07 21:24:39 -0400

    using stateful lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,7 +20,15 @@ const Expenses = (props) => {
           onFilterByYear={filterByYearHandler}
           selected={filteredYear} //Two-way binding
         />
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -41,7 +49,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
